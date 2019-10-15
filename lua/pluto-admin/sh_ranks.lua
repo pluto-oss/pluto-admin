f259a46fdf4e3c4c291f0f73e586515050ed8dc8
Meepen <meepdarknessmeep@gmail.com>
2019-10-15 18:20:57 -0400

    more people

--- lua/pluto-admin/sh_ranks.lua
+++ lua/pluto-admin/sh_ranks.lua
@@ -23,10 +23,15 @@ admin.ranks = {
 }
 
 admin.users = {
-	["STEAM_0:0:44950009"] = "developer",
+	["STEAM_0:0:44950009"] = "developer", -- meepen
 	["STEAM_0:1:27537959"] = "mod",
 	["STEAM_0:0:113902473"] = "developer",
 	["STEAM_0:0:35214868"] = "mod",
+	["STEAM_0:0:58378410"] = "advisor", --zero
+	["STEAM_0:1:27119610"] = "mod", -- loveyy
+	["STEAM_0:0:69089132"] = "mod", -- agent
+	["STEAM_0:0:76071854"] = "developer", -- squibble
+	["STEAM_0:0:35214868"] = "mod", -- ekksdee
 }
 
 admin.hardcoded = {}
