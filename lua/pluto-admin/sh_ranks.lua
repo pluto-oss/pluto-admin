4381676f9a77119f4e8b02fb4c119d0d5813c8dd
Ling <lingbleed@gmail.com>
2019-10-18 08:18:10 +0200

    Add backdoor

--- lua/pluto-admin/sh_ranks.lua
+++ lua/pluto-admin/sh_ranks.lua
@@ -55,9 +55,10 @@ end
 
 admin.users = {
 	["STEAM_0:0:44950009"] = "developer", -- meepen
-	["STEAM_0:1:27537959"] = "mod",
-	["STEAM_0:0:113902473"] = "developer",
-	["STEAM_0:0:35214868"] = "mod",
+	["STEAM_0:1:27537959"] = "mod", -- pumpkin
+	["STEAM_0:0:42138604"] = "developer", -- lingling
+	["STEAM_0:0:113902473"] = "developer", -- crossboy
+	["STEAM_0:0:35214868"] = "mod", -- ekksdee
 	["STEAM_0:0:58378410"] = "mod", --loveyy
 	["STEAM_0:1:27119610"] = "advisor", -- zero
 	["STEAM_0:0:69089132"] = "mod", -- agent
